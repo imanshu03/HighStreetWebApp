4661267066a5863aeb057cb8ca54653151418716
imanshu rathore <imanshurathore@gmail.com>
2021-12-20 00:18:17 +0530

    fixed build error

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
-}
-
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    // Process a POST request
-    console.log(req)
-  } else {
-    // Handle any other HTTP method
-  }
+  res.status(200).json({ name: 'John Doe' });
 }
