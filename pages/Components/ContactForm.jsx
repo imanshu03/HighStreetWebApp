a28aef542755036ff3ecf56a4b098ddc77b5234c
toshal lubana <toshal.t1@gmail.com>
2021-12-19 13:40:50 +0530

    adding contact form

--- pages/Components/ContactForm.jsx
+++ pages/Components/ContactForm.jsx
@@ -1,5 +1,7 @@
-import react from 'react';
+import React, { useRef, useEffect, useContext } from 'react';
 import GoogleMapReact from 'google-map-react';
+import { ObserverContext } from '../../utils/ObserverContext';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
 import * as yup from 'yup';
 
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
@@ -26,92 +28,81 @@ const ContactForm = () => {
     email: yup.string().email().required('Email is a required field'),
   });
 
+  const ref = useRef(null);
+  const observer = useContext(ObserverContext);
+
+  useEffect(() => {
+    if (ref.current && observer) {
+      observer.observe(ref.current);
+    }
+  }, [ref, observer]);
+
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-sm text-center">
-          <h1 className="div-heading display-4">Contact US</h1>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-6">
-          <h5>
-            Address:{' '}
-            <small className="text-muted">High Streat, Peer Muchalla</small>
-          </h5>
-          <h5>
-            Email: <small className="text-muted">Shubham@gmail.com</small>
-          </h5>
-          <h5>
-            Contact:{' '}
-            <small className="text-muted">
-              +91 77009696967 || +91 7837770661
-            </small>
-          </h5>
-          <p>
-            <div style={{ height: '400px', width: '100%' }}>
-              <GoogleMapReact
-                bootstrapURLKeys={{ key: '' }}
-                defaultCenter={constraint.center}
-                defaultZoom={constraint.zoom}
-              >
-                <AnyReactComponent
-                  lat={59.955413}
-                  lng={30.337844}
-                  text="My Marker"
-                />
-              </GoogleMapReact>
-            </div>
-          </p>
-        </div>
-        <div className="col-md-6">
-          <form className="mt-6">
-            <div className="form-group mb-3">
+    <Container
+      className="grey py-5 transition-wrapper"
+      id="contact-us"
+      fluid
+      ref={ref}
+      data-func="show"
+      data-classname="transition-up"
+    >
+      <Row className="row py-3">
+        <Col>
+          <h1 className="display-4 text-center">Contact Us</h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col md={6} sm={12} className="mt-3">
+          <Row>
+            <h5>
+              Address:{' '}
+              <small className="text-muted">High Street, Peer Muchalla</small>
+            </h5>
+            <h5>
+              Email: <small className="text-muted">Shubham@gmail.com</small>
+            </h5>
+            <h5>
+              Contact:{' '}
+              <small className="text-muted">
+                +91 77009696967 || +91 7837770661
+              </small>
+            </h5>
+          </Row>
+          <Row>
+            <Container style={{ height: '400px' }} className="w-100">
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3432.1020759918065!2d76.85372301564612!3d30.659253996220155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390f95f194d5ca13%3A0x28f78ba6fdceed9b!2sHigh%20Street%2C%20Commercial%20Showrooms%20at%20Peer%20muchulla%2C%20Sector%2020%20Panchkula!5e0!3m2!1sen!2sin!4v1639038140568!5m2!1sen!2sin&z=20"
+                allowFullScreen
+                loading="lazy"
+                className="w-100 h-100 map-location"
+              ></iframe>
+            </Container>
+          </Row>
+        </Col>
+        <Col md={6} sm={12} className="mt-3">
+          <Form className="w-100 h-100 d-flex flex-column justify-content-between">
+            <Form.Group>
               {/* <label>FirstName</label> */}
-              <input
-                type="name"
-                name="fullName"
-                label="fullName"
-                className="form-control"
-                id="exampleInputFirstName"
-                placeholder="Full Name"
-              />
-            </div>
-            <div className="form-group mb-3">
+              <Form.Control type="name" placeholder="Full Name" />
+            </Form.Group>
+            <Form.Group className="mt-3">
               {/* <label>Mobile</label> */}
-              <input
-                type="mobile"
-                className="form-control"
-                id="mobileInput"
-                aria-describedby="mobileHelp"
-                placeholder="Mobile Number"
-              />
-            </div>
-            <div className="form-group  mb-3">
+              <Form.Control type="mobile" placeholder="Mobile Number" />
+            </Form.Group>
+            <Form.Group className="mt-3">
               {/* <label>Email Address</label> */}
-              <input
-                type="email"
-                className="form-control"
-                id="exampleInputEmail1"
-                aria-describedby="emailHelp"
-                placeholder="Email Address"
-              />
-            </div>
-            <div className="form-group  mb-3">
-              <textarea
-                className="form-control"
-                aria-label="With textarea"
-                rows="10 "
-                placeholder="Message"
-              ></textarea>
-            </div>
-            <button type="submit" className="btn btn-warning btn-lg btn-block">
+              <Form.Control type="email" placeholder="Email Address" />
+            </Form.Group>
+            <Form.Group className="mt-3">
+              <Form.Control rows="10" placeholder="Message" as="textarea" />
+            </Form.Group>
+            <Button type="submit" variant="primary" className="mt-3 w-100">
               Submit
-            </button>
-          </form>
-        </div>
-      </div>
-    </div>
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
